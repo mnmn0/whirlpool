6af25d309fe59b94b7b5d506d8ebb09f4c455be1
mnmn0 <63435902+mnmn0@users.noreply.github.com>
2024-02-15 22:29:40 +0900

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import NavBar from '@/app/_components/nav-bar/NavBar';
+import SideBar from '@/app/_components/side-bar/SideBar';
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "whirlpool",
+  description: "progress management app",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <div>
+          <SideBar />
+          <div className="sm:ml-64 min-h-svh">
+            <NavBar />
+            {children}
+          </div>
+        </div>
+
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+import NavBar from '@/app/_components/nav-bar/NavBar';
+
+export default function Home() {
+  return (
+    <main className="flex min-h-fit flex-col items-center justify-between p-24">
+    </main>
+  );
+}
