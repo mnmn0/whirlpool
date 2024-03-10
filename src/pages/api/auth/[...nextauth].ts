c0b6a8e72ab0b2e79933cacf24e5ada3a31fccfb
mnmn0 <63435902+mnmn0@users.noreply.github.com>
2024-03-10 22:28:17 +0900

    next-authを修正

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,15 +1,40 @@
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
-import { User } from "next-auth"
-import { JWT } from "next-auth/jwt"
+import NextAuth, { NextAuthOptions, Session } from 'next-auth';
+import GoogleProvider, { GoogleProfile } from 'next-auth/providers/google';
+import { JWT } from 'next-auth/jwt';
+
+interface CustomUser {
+  id: string;
+  name: string;
+  email: string;
+  image: string;
+}
 
 export const authOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      profile: (profile: GoogleProfile) => {
+        return {
+          id: profile.sub,
+          name: profile.name,
+          email: profile.email,
+          image: profile.picture,
+        }
+      },
     })
   ],
+  callbacks: {
+    session: ({ session, token }: { session: Session, token: JWT}) => ({
+      ...session,
+      user: {
+        id: token.sub,
+        name: token.name,
+        email: token.email,
+        image: token.picture,
+      } as CustomUser,
+    }),
+  },
   secret: process.env.NEXTAUTH_SECRET,
 }
 
